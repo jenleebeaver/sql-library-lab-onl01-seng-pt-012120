b342c13e2533517344044a2beb28eec07ab92245
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-27 00:36:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT Characters.species, COUNT(*) FROM Characters"
+  "SELECT Characters.species, COUNT(*) FROM Characters GROUP BY species ORDER BY COUNT(species)"
 end
 
 def select_name_and_series_subgenres_of_authors
